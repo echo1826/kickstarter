a5396162603fdd710550a9565f1ebe956ae4d792
Ethan <ethan.cho1826@gmail.com>
2022-08-04 16:53:36 -0500

    creating a request on campaign, vote for approval on a request, finalizing the request to complete

--- contract/Campaign.sol
+++ contract/Campaign.sol
@@ -8,14 +8,18 @@ contract Campaign {
     struct Request {
         string description;
         uint256 value;
-        address recipient;
+        address payable recipient;
         bool complete;
+        uint256 approvalCount;
+        mapping(address => bool) approvals;
     }
 
     address public manager;
-    address[] approvers;
+    uint256 numRequests;
+    mapping(address => bool) approvers;
     uint256 public minimumContribution;
-    Request[] public requests;
+    uint256 public approversCount;
+    mapping(uint256 => Request) requests;
 
     constructor(uint256 minContribution) {
         manager = msg.sender;
@@ -30,14 +34,48 @@ contract Campaign {
     function contribute() public payable {
         require(msg.value >= minimumContribution);
 
-        approvers.push(msg.sender);
+        // approvers.push(msg.sender);
+        approvers[msg.sender] = true;
+        approversCount++;
     }
 
-    function getApprovers() public view returns (address[] memory) {
-        return approvers;
+    function createRequest(
+        string memory description,
+        uint256 value,
+        address recipient
+    ) public restricted {
+        // CANNOT CONSTRUCT A STRUCT WITH A NESTED MAPPING IN IT ANYMORE!!
+        // Request memory newRequest = Request({
+        //     description: description,
+        //     value: value,
+        //     recipient: recipient,
+        //     complete: false,
+        //     approvalCount: 0
+        // });
+
+        Request storage request = requests[numRequests++];
+        request.description = description;
+        request.value = value;
+        request.recipient = payable(recipient);
+        request.complete = false;
+        request.approvalCount = 0;
     }
 
-    function createRequest() restricted public {
+    function approveRequest(uint256 key) public {
+        Request storage request = requests[key];
 
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint256 key) public restricted {
+        Request storage request = requests[key];
+        require(!request.complete);
+        require(request.approvalCount > (approversCount / 2));
+        request.recipient.transfer(request.value);
+        request.complete = true;
     }
 }
