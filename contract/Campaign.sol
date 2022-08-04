5b2bb94c9c523d2075f724d740b5bc17a23b92f4
Ethan <ethan.cho1826@gmail.com>
2022-08-04 15:38:11 -0500

    created request object, array of requests on the contract, restricted function modifier

--- contract/Campaign.sol
+++ contract/Campaign.sol
@@ -1,16 +1,30 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.9;
+
 // linter warnings (red underline) about pragma version can igonored!
 
 contract Campaign {
+    struct Request {
+        string description;
+        uint256 value;
+        address recipient;
+        bool complete;
+    }
+
     address public manager;
     address[] approvers;
-    uint public minimumContribution;
-    
-    constructor(uint minContribution) {
+    uint256 public minimumContribution;
+    Request[] public requests;
+
+    constructor(uint256 minContribution) {
         manager = msg.sender;
         minimumContribution = minContribution;
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
     }
 
     function contribute() public payable {
@@ -19,7 +33,11 @@ contract Campaign {
         approvers.push(msg.sender);
     }
 
-    function getApprovers() public view returns(address[] memory) {
+    function getApprovers() public view returns (address[] memory) {
         return approvers;
+    }
+
+    function createRequest() restricted public {
+
     }
 }
